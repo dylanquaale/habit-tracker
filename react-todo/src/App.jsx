e468e120440140142cbd9f0e83cfc0430cba6247
dylanquaale <dylanquaale33@gmail.com>
2023-08-04 14:24:43 -0500

    added heroIcons

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 
+// custom components
+import CustomForm from './components/CustomForm'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-        <div className='App'>
-
+        <div className='container'>
+          <header>
+            <h1>Todo App</h1>
+          </header>
+          <CustomForm />
         </div>
         
     
--- react-todo/src/components/CustomForm.jsx
+++ react-todo/src/components/CustomForm.jsx
@@ -1,18 +1,41 @@
+import { PlusIcon } from '@heroicons/react/24/solid'
+
+
+
 const CustomForm = () => {
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+  };
 
-    const handleFormSubmit = (e) => {
-        e.preventDefault();
-        console.log(e)
-    }
-    return (
-        <form 
-        className='todo'
-        onSubmit={handleFormSubmit}
-        
-        >
+  return (
+    <form className="todo" onSubmit={handleFormSubmit}>
+      <div className="wrapper">
+        <input
+          type="text"
+          id="task"
+          className="input"
+          value={task}
+          onInput={(e) => setTask(e.target.value)}
+          required
+          autoFocus
+          maxLength={60}
+          placeholder="Enter Task"
+        />
+        <label 
+        htmlFor=""
+        className='label'
+        >Enter Task</label>
+      </div>
+      <button 
+      className='btn'
+      aria-label='Add Task'
+      type='submit'
+      >
+       <PlusIcon/>
+      </button>
+    </form>
+  );
+};
 
-        </form>
-    )
-}
-
-export default CustomForm
+export default CustomForm;
